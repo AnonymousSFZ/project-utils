bf93b8c5db9b768651f8fad52ff71b66fb4980cb
Henry <33919933+AnonymousSFZ@users.noreply.github.com>
2019-07-25 10:41:53 +0100

    Update xml-processing.py

--- xml-processing.py
+++ xml-processing.py
@@ -4,22 +4,17 @@ import xml.etree.ElementTree as ET
 from os import listdir, rename, mkdir
 from os.path import isfile, join, exists
 
-def add_prefix(string, prefix):
-    return string.split('.j')[0] + prefix + '.jpg'
-
-src = 'Labels/Images1.0/003'
-prefixs = ['(bright30)', '(dark30)', '(gauss)']
-
-for prefix in prefixs:
-        if not exists(src + prefix):
-            mkdir(src + prefix)
+src = 'Images1.0/003'
+path = 'images/train'
 
 onlyfiles = [f for f in listdir(src) if isfile(join(src, f))]
 for file in onlyfiles:
     if re.match(r'.*\.xml', file, re.I):
-        for prefix in prefixs:
-            tree = ET.parse(src + '/' + file)
-            root = tree.getroot()
-            root[1].text = add_prefix(root[1].text, prefix)
-            root[2].text = add_prefix(root[2].text, prefix)
-            tree.write(src + '/' + file.split('.')[0] + prefix + '.xml')
+        tree = ET.parse(src + '/' + file)
+        root = tree.getroot()
+        filename = file.split('.xml')[0] + '.jpg'
+        # root[1] - file name
+        root[1].text = filename
+        # root[2] - path
+        root[2].text = path + '/' + filename
+        tree.write(src + '/' + file)
